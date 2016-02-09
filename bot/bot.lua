706e48ee4fa3c9ea5302e607a5f1e0e6f0d9f1e7
alirezanile <alirezaaghajani99@gmail.com>
2016-02-09 19:58:23 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {172286224},
+    sudo_users = {94727089},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
